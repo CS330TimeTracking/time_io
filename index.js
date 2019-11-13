073b709f047ad93d65b593dec12b3f1489fcdf4a
Alex van der Heijden <vdhalex@gmail.com>
2019-11-13 14:45:16 -0600

    can now add multiple activities, track time, pause resume (activity name activity doesn't work though)

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ function startTimer() {
     return;
   }
 
-  var currTime = new Date();
-  activityData.activityName = activity;
+  if (activityData[activity]) {
+    alert("Activity already exists, pick another name or remove other activity.")
+    return;
+  }
 
-  activityData.activity = {
+  var currTime = new Date();
+  activityData[activity] = {
     'priotity': priotity,
     'elapsedTime': 0,
     'startTime': {
@@ -21,7 +24,37 @@ function startTimer() {
       'minutes': currTime.getMinutes,
       'seconds': currTime.getSeconds
     },
+    'intervalId': null,
   };
 
-  timers.insertAdjacentHTML("afterend", `<div id="new-timer">${activityData.activityName}: ${activityData.activity.elapsedTime}</div>`);  
+  timers.insertAdjacentHTML("afterend", `<div class="new-timer">${activity}: <span id="${activity}">00:00:00</span><br/><button id="${activity}-pp" onclick="togglePausePlay('${activity}')">Resume</button></div>`);
+  togglePausePlay(activity); 
+}
+
+function togglePausePlay(activity) {
+  var pp = document.getElementById(`${activity}-pp`);
+  if (pp.innerHTML == 'Pause') {
+    clearInterval(activityData[activity].intervalId);
+    pp.innerHTML = 'Resume';
+  } else {
+    pp.innerHTML = 'Pause';
+    activityData[activity].intervalId = setInterval(() => {
+      activityData[activity].elapsedTime++;
+      document.getElementById(activity).innerHTML = `${timeFormat(activityData[activity].elapsedTime)}`
+    }, 1000);
+  } 
+}
+
+function timeFormat(s) {
+  var seconds = s % 60;
+  var minutes = (s - seconds) / 60;
+  var hours = Math.floor(minutes / 60);
+  minutes = minutes - (hours * 60);
+
+  // this is gross but pressed for time
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+
+  return `${hours}:${minutes}:${seconds}`;
 }
